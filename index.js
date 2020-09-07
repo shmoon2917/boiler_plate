8242ceb262f019824243489b7554a2f19964c788
shmoon2917 <shmoon2917@naver.com>
2020-09-07 22:15:25 +0900

    로그인 및 인증 API 기능 구현

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
 const config = require('./config/key');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 
 const app = express();
 const port = 5000;
@@ -23,17 +25,73 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('Hello World! hey');
 });
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   // 회원 가입 시 필요한 정보들을 client 에서 가져오면
   // 그 것들을 데이터 베이스에 넣어준다.
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post('/api/users/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에 있는지 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '이메일에 해당하는 유저가 없습니다.',
+      });
+    }
+
+    // 이메일이 데이터 베이스에 있다면 비밀번호를 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다',
+        });
+      }
+      // 비밀번호까지 맞다면 Token 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // 토큰을 저장한다.
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 미들웨어를 통과했다면 Authentication 이 True 라는 뜻.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user.id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
       success: true,
     });
   });
